f5086236cd7b5be9ad478b0db654ceb31117e946
KiritMand <kiritmand@gmail.com>
2024-08-17 04:08:13 -0400

    52 min video, start of flashcards

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,26 @@
+import { SignIn } from "@clerk/nextjs";
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+
+export default function SignUpPage() {
+    return (
+        <Container maxWidth='sm'>
+            <AppBar position='static' sx={{backgroundColor: '#3f51b5'}}>
+                <Toolbar>
+                    <Typography variant="h6" sx={{flexGrow:1,}}>Flashcard AI</Typography>
+                    <Button color="inherit">
+                        <Link href="/sign-in" passHref>Login</Link>
+                    </Button>
+                    <Button color="inherit">
+                        <Link href="/sign-up" passHref>Sign Up</Link>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <Box display = 'flex' flexDirection='column' alignItems='center' justifyContent='center'>
+                <Typography>Sign In</Typography>
+                <SignIn/>
+            </Box>
+        </Container>
+    )
+}
