38eed2c84908ce88eb7972907d79dba8da88f654
KiritMand <kiritmand@gmail.com>
2024-08-17 17:43:30 -0400

    1:34 video, save functionality added

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -1,28 +1,35 @@
 'use client'
-import { db } from "@/firebase"
-import { useUser } from "@clerk/nextjs"
-import { Box, Button, Card, CardActionArea, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Paper, TextField, Typography } from "@mui/material"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
-import { doc, collection, setDoc, getDoc, writeBatch } from "firebase/firestore"
 
-export default function Generate() {
+import { useUser } from "@clerk/nextjs"
+import { useEffect, useState } from "react"
+import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import { db } from "@/firebase"
+
+import { useSearchParams } from "next/navigation"
+import { Box, Button, Card, CardActionArea, CardContent, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Paper, TextField, Typography } from "@mui/material"
+
+export default function Flashcard(){
     const {isLoaded, isSignedIn, user} = useUser()
     const [flashcards, setFlashcards] = useState([])
     const [flipped, setFlipped] = useState([])
-    const [text, setText] = useState('')
-    const [name, setName] = useState('')
-    const [open, setOpen] = useState(false)
-    const router = useRouter()
 
-    const handleSubmit = async () => {
-        fetch('api/generate', {
-            method: 'POST',
-            body: text,
-        })
-            .then((res) => res.json())
-            .then((data) => setFlashcards(data))
-    }
+    const searchParams = useSearchParams()
+    const search = searchParams.get('id')
+
+    useEffect(() => {
+        async function getFlashcard() {
+            if (!search || !user) return
+            const colRef = collection(doc(collection(db, 'users'), user.id), search)
+            const docs = await getDocs(colRef)
+            const flashcards = []
+
+            docs.forEach((doc) => {
+                flashcards.push({id: doc.id, ... doc.data()})
+            })
+            setFlashcards(flashcards)
+        }
+        getFlashcard()
+    }, [user, search])
 
     const handleCardClick = (id) => {
         setFlipped((prev) => ({
@@ -30,65 +37,16 @@ export default function Generate() {
             [id]: !prev[id],
         }))
     }
-
-    const handleOpen = () => {
-        setOpen(true)
-    }
-    const handleClose = () => {
-        setOpen(false)
-    }
-    const saveFlashcards = async () =>{
-        if (!name) {
-            alert('Please enter a name')
-            return
-        }
-
-         // Ensure user is loaded and signed in
-         if (!isLoaded || !isSignedIn || !user) {
-            alert('User is not signed in.')
-            return
-        }
-        const batch = writeBatch(db)
-        const userDocRef = doc(collection(db, 'users'), user.id)
-        const docSnap = await getDoc(userDocRef)
-
-        if (docSnap.exists()) {
-            const collections = docSnap.data().flashcards || []
-            if (collections.find((f) => f.name === name)){
-                alert('Flashcard collection with the same name already exists.')
-                return
-            } else {
-                collections.push({name})
-                batch.set(userDocRef, {flashcards: collections}, {mersge: true})
-            }
-        } else {
-            batch.set(userDocRef, {flashcards: [{name}]})
-        }
-
-        const colRef = collection(userDocRef, name)
-        flashcards.forEach((flashcard) => {
-            const cardDocRef = doc(colRef)
-            batch.set(cardDocRef, flashcard)
-        })
-
-        await batch.commit()
-        handleClose()
-        router.push('/flashcards')
+    // Ensure user is loaded and signed in
+    if (!isLoaded || !isSignedIn) {
+        
+        return<></>
     }
 
-    return <Container maxWidth='md'>
-        <Box sx={{mt:4, mb:6, display:'flex', flexDirections:'column', alignItems:'center'}}>
-            <Typography>Generate Flashcards</Typography>
-            <Paper sx={{p:4, width:'100%'}}>
-                <TextField value = {text} onChange={(e) => setText(e.target.value)} label="Enter text" fullWidth multiline rows={4} variant="outlined" sx={{mb:2 }}/> 
-                <Button variant='contained' color = 'primary' onClick={handleSubmit} fullWidth>
-                    {' '}
-                    Submit
-                </Button>
-            </Paper>
-        </Box>
-
-        {flashcards.length > 0 && (
+    return (
+        <Container maxWidth="100vw">
+            <Grid container spacing={3} sx={{mt:4}}>
+            {flashcards.length > 0 && (
             <Box sx={{mt:4}}>
                 <Typography>Flashcards Preview</Typography>
                 <Grid container spacing = {3}>
@@ -145,26 +103,9 @@ export default function Generate() {
                         </Grid>
                     ))}
                 </Grid>
-                <Box sx = {{mt:4, display:'flex', justifyContent:'center'}}>
-                    <Button variant="contained" color='secondary' onClick={handleOpen}>
-                        Save
-                    </Button>
-                </Box>
             </Box>
         )}
-
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Save Flashcards</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    Please enter name for flashcards collection
-                </DialogContentText>
-                <TextField autoFocus margin='dense' label='Collection Name' type="text" fullWidth value={name} onChange={(e) => setName(e.target.value)} variant='outlined' />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={saveFlashcards}>Save</Button>
-            </DialogActions>
-        </Dialog>
-    </Container>
+            </Grid>
+        </Container>
+    )
 }
